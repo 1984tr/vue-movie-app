b3b88479ae4f40b6483242e2b7574308e3680740
tris84 <jollytris@gmail.com>
2020-08-27 21:14:16 +0900

    search component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-text-field
+      v-model="title"
+      @keypress.enter="searchMovies"
+      outlined>
+      <template v-slot:prepend-inner>
+        <v-icon>search</v-icon>
+      </template>
+      <template v-slot:append>
+        <v-progress-circular
+          v-if="loading"
+          color="primary"
+          size="24"
+          indeterminate />
+      </template>
+    </v-text-field>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      loading: false
+    }
+  },
+  methods: {
+    searchMovies () {
+      console.log('search!!')
+    }
+  }
+}
+</script>
